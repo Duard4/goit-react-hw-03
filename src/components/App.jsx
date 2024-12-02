4a4689ab088f15ef894482e8e591751215f73725
Eduard <parhomenkoeduard75@gmail.com>
2024-12-02 19:03:38 +0200

    feat: created contact list widget and cleaned up template code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,42 @@
 import "./App.css";
-import userData from "../data/userData.json";
-import friends from "../data/friends.json";
-import transactions from "../data/transactions.json";
-import Profile from "./Profile/Profile";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import initContacts from "../data/contacts.json";
+import ContactForm from "./ContactForm/ContactForm";
+import SearchBox from "./SearchBox/SearchBox";
+import ContactList from "./ContactList/ContactList";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
 export default function App() {
+    const [contacts, setContacts] = useState(() => {
+        const savedContacts = window.localStorage.getItem("savedContacts");
+        return savedContacts ? JSON.parse(savedContacts) : initContacts;
+    });
+    const [search, setSearch] = useState("");
+
+    const addContact = (newContact) => {
+        setContacts((prevContacts) => [
+            ...prevContacts,
+            { id: nanoid(), ...newContact },
+        ]);
+    };
+    const handleDelete = (contactId) => {
+        console.log(contactId);
+        setContacts(contacts.filter((contact) => contact.id !== contactId));
+    };
+    useEffect(() => {
+        window.localStorage.setItem("savedContacts", JSON.stringify(contacts));
+    }, [contacts]);
+
+    const visibleContacts = contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <>
-            <Profile
-                name={userData.username}
-                tag={userData.tag}
-                location={userData.location}
-                image={userData.avatar}
-                stats={userData.stats}
-            />
-            <FriendList friends={friends} />
-            <TransactionHistory items={transactions} />
+            <h1>Phonebook</h1>
+            <ContactForm onAdd={addContact} />
+            <SearchBox onSearch={setSearch} value={search} />
+            <ContactList contacts={visibleContacts} onDelete={handleDelete} />
         </>
     );
 }
